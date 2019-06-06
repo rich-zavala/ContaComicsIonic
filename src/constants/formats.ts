ccd6b86c264b97c27e0f750d7993b24fd6db2396
Ricardo Zavala <rzavala@tibco-support.com>
2019-06-06 12:38:19 -0500

    Date formats and Español

--- src/constants/formats.ts
+++ src/constants/formats.ts
@@ -1,3 +1,3 @@
 export const DATE_FORMAT = "YYYY-MM-DD";
-export const DATE_FORMAT_READ = "LL";
-export const DATE_FORMAT_READ_TIME = "LLLL";
+export const DATE_FORMAT_READ = "dddd [-] MMMM D[,] YYYY";
+export const DATE_FORMAT_READ_TIME = "dddd [-] MMMM D[,] YYYY [-] LT";
