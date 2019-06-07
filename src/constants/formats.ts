bbdbd0aa6996a311dde0978865631627c2987670
Ricardo Zavala <rzavala@tibco-support.com>
2019-06-06 19:39:59 -0500

    Date Formats updates

--- src/constants/formats.ts
+++ src/constants/formats.ts
@@ -1,3 +1,3 @@
 export const DATE_FORMAT = "YYYY-MM-DD";
-export const DATE_FORMAT_READ = "dddd [-] MMMM D[,] YYYY";
-export const DATE_FORMAT_READ_TIME = "dddd [-] MMMM D[,] YYYY [-] LT";
+export const DATE_FORMAT_READ = "ddd [-] MMM D[,] YYYY";
+export const DATE_FORMAT_READ_TIME = "ddd [-] MMM D[,] YYYY [-] LT";
